b46ece53deed24e3ae2832ff799b5138188ecd05
Zhenghui <zwan1312@bu.edu>
2019-11-16 16:17:12 -0500

    working router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app>
     <router-view />
-
-
     <v-content>
       <HelloWorld/>
     </v-content>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>ABOUT</h1>
 </template>
+
+<script>
+export default {
+  name: 'about',
+
+}
+</script>
