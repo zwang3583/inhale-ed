2f6969778b96024eefd30088fe121ed3a50cfa23
Hannah Huang <yunhanh@bu.edu>
2019-11-16 17:19:49 -0500

    add calendar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
   <v-app>
     <router-view />
-
-
     <v-content>
-      <HelloWorld/>
+      <Calendar />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Calendar from "./components/Calendar";
 
+// import navigationBar from "@/components/NavigationBar.vue";
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    Calendar
   },
-
   data: () => ({
     //
-  }),
+  })
 };
+
+// import HelloWorld from './components/HelloWorld';
+
+// export default {
+//   name: 'App',
+
+//   components: {
+//     HelloWorld,
+//   },
+
+//   data: () => ({
+//     //
+//   }),
+// };
 </script>
