e62e9009706b0c208115a7ffac15c3bfd8a57fbf
Sheila Jimenez Morejon <jimenezh@bu.edu>
2019-11-16 15:59:12 -0500

    routing done

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+
+<script>
+
+  export default {
+    name: Sidebar,
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' },
+        ],
+      }
+    },
+  }
+</script>
