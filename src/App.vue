2ac1c6303b81fbc20e2fbe3fe4b5c5e20fd8b56d
Zhenghui <zwan1312@bu.edu>
2019-11-16 16:49:47 -0500

    updated layout

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <v-app>
     <router-view />
-    <v-content>
-      <HelloWorld/>
-    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    //
   },
 
   data: () => ({
